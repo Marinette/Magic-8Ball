efb6944224ad81e763ebbd03032d004b18cf15fc
meggiechen <Egg@meggies-macbook-air.local>
2018-06-20 14:26:04 -0400

    Working basic Dicee app, randomizes when button pushed or phone shaken

--- Magic 8Ball/ViewController.swift
+++ Magic 8Ball/ViewController.swift
@@ -2,14 +2,18 @@
 //  ViewController.swift
 //  Magic 8Ball
 //
-//  Created by Meggie Chen on 18-06-20.
+//  Created by Marinette Chen on 18-06-20.
 //  Copyright © 2018 Marinette Chen. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController {   
+    let imageArray = [ "ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    var ballChoice: Int = 0
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func rollOnButtonPressed(_ sender: UIButton) {
+        rerollNum()
+    }
+    
+    func rerollNum() {
+       ballChoice = Int(arc4random_uniform(4))
+        
+        imageView.image = UIImage(named: imageArray[ballChoice])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        rerollNum()
+    }
 
+    
 }
 
