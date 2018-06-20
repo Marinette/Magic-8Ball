dd6516b96b198fc9a96131ccf5553b0d2c197c4f
meggiechen <Egg@meggies-macbook-air.local>
2018-06-20 13:29:34 -0400

    Initial Commit

---
+++ Magic 8Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8Ball
+//
+//  Created by Meggie Chen on 18-06-20.
+//  Copyright © 2018 Marinette Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
